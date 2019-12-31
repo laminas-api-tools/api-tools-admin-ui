9f036d96c682d2f8e09bde05dfe8b64ba691576c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:54:52 -0600

    Rewriting as Laminas Project package

--- Module.php
+++ Module.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * @link      http://github.com/zfcampus/zf-apigility-admin-ui for the canonical source repository
- * @copyright Copyright (c) 2013-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas-api-tools/api-tools-admin-ui for the canonical source repository
+ * @copyright https://github.com/laminas-api-tools/api-tools-admin-ui/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas-api-tools/api-tools-admin-ui/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZF\Apigility\Admin\Ui;
+namespace Laminas\ApiTools\Admin\Ui;
 
 class Module
 {
